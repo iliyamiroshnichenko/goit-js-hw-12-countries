596963ea8618fd687b6e64162838ea65a2d43f19
Iliya <iliyamiroshnichenko88@gmail.com>
2021-01-26 21:22:55 +0200

    half work

--- src/index.js
+++ src/index.js
@@ -1,1 +1,20 @@
 import './sass/styles.scss';
+import refs from './js/refs';
+import fetchCountries from './js/fetchCountries';
+import debounce from 'lodash.debounce';
+import addMarkup from './js/add-markup';
+
+refs.countryInput.addEventListener('input', debounce(countryInputHandler, 500));
+refs.countryInput.addEventListener('focus', clearCountryInput);
+
+function countryInputHandler(event) {
+  fetchCountries(event.target.value).then(countries => {
+    console.log(countries);
+    refs.markup.innerHTML = '';
+    addMarkup(countries);
+  });
+}
+
+function clearCountryInput(event) {
+  event.target.value = '';
+}
---
+++ src/js/fetchCountries.js
@@ -0,0 +1,10 @@
+export default function fetchCountries(searchQuery) {
+  const url = `https://restcountries.eu/rest/v2/name/${searchQuery}`;
+
+  return fetch(url)
+    .then(res => res.json())
+    .then(data => {
+      return data;
+    })
+    .catch(error => console.log(error));
+}
